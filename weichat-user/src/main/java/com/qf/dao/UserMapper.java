e801340b6941d6346b97dcf9c6265d799de9c1f3
yuntong <57180569+CrazygodT@users.noreply.github.com>
2021-01-08 12:07:14 +0800

    评论模块,搜索

--- weichat-user/src/main/java/com/qf/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.qf.dao;
-
-import com.qf.pojo.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserMapper extends JpaRepository<User,Integer> {
-
-    User findByUsername(String username);
-}
